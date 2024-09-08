ba63fc0cd4bcf88bac35c9028e235ca9bfa3aca8
jacob015 <jacob015@naver.com>
2024-09-08 20:51:00 +0900

    Weapon에 필요한 클래스 구성

---
+++ Source/UniversityProject/Character/Weapon/UPWeapon.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WeaponSwingTrajectoryData.h"
+#include "GameFramework/Actor.h"
+#include "UPWeapon.generated.h"
+
+UCLASS()
+class UNIVERSITYPROJECT_API AUPWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AUPWeapon();
+
+	// 궤적 데이터를 저장할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	AWeaponSwingTrajectoryData* SwingTrajectoryData;
+	
+	// 공격 중 프레임마다 호출되는 함수
+	void PerformSwing(int32 CurrentFrameIndex);
+	
+protected:
+	// 메쉬
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+	USkeletalMeshComponent* WeaponMesh;
+};
