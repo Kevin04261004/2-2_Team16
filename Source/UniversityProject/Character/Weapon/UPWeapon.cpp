bacc7cbcde70459d44ec587e4f46f06a45d62994
Kevin04261004 <kdystudy0426@gmail.com>
2024-09-11 10:05:24 +0900

    doxygen파일 생성중

--- Source/UniversityProject/Character/Weapon/UPWeapon.cpp
+++ Source/UniversityProject/Character/Weapon/UPWeapon.cpp
@@ -20,45 +20,6 @@ AUPWeapon::AUPWeapon()
 	BeforeSocketLocationArray.Init(FVector(0,0,0), CollisionSocketNameArray.Num());
 }
 
-void AUPWeapon::PerformSwing(int32 CurrentFrameIndex)
-{
-	// 궤적 데이터가 있고, 현재 프레임 데이터가 존재하는지 확인
-	if (SwingTrajectoryData && SwingTrajectoryData->SwingFrameData.IsValidIndex(CurrentFrameIndex))
-	{
-		// 현재 프레임 데이터를 가져옴 읽기전용, 구조체라서 참조로 가져옴
-		const FWeaponSwingFrameData& FrameData = SwingTrajectoryData->SwingFrameData[CurrentFrameIndex];
-
-		// 트레이스로 충돌을 감지
-		FHitResult HitResult;
-		FVector Start = FrameData.StartPosition;
-		FVector End = FrameData.EndPosition;
-
-		// 라인 트레이스 사용 가벼우니깐.. Single과 Multi 둘 중 어느것을 사용할지는 추후 게임성따라 결정 일단은 Single
-		// 트레이스 채널 하나 만들어야됨 WeaponTrace라는 이름으로
-		bool bHit = GetWorld()->LineTraceSingleByChannel(
-			HitResult,
-			Start,
-			End,
-			ECC_Visibility,
-			FCollisionQueryParams(FName(TEXT("WeaponTrace")), false, this)
-		);
-
-		// 충돌이 감지된 경우 처리
-		if (bHit)
-		{
-			AActor* HitActor = HitResult.GetActor();
-			if (HitActor)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("Hit Actor: %s"), *HitActor->GetName());
-				// 나중에 로직 구현
-			}
-		}
-
-		// 충돌 범위 디버그
-		DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 1.0f, 0, 2.0f);
-	}
-}
-
 void AUPWeapon::NotifyAttackCheck()
 {
 	CheckCollisionSockets();
--- Source/UniversityProject/Character/Weapon/UPWeapon.h
+++ Source/UniversityProject/Character/Weapon/UPWeapon.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "WeaponSwingTrajectoryData.h"
 #include "GameFramework/Actor.h"
 #include "UPWeapon.generated.h"
 
@@ -14,13 +13,6 @@ class UNIVERSITYPROJECT_API AUPWeapon : public AActor
 
 public:
 	AUPWeapon();
-
-	// 궤적 데이터를 저장할 변수
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
-	AWeaponSwingTrajectoryData* SwingTrajectoryData;
-	
-	// 공격 중 프레임마다 호출되는 함수
-	void PerformSwing(int32 CurrentFrameIndex);
 
 	// 애니메이션에서 매 프레임마다 호출하는 Notify함수
 	void NotifyAttackCheck();
