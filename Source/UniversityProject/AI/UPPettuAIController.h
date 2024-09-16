a16e838954f7cd083abd5ba9fd32e584509b9198
jacob015 <jacob015@naver.com>
2024-09-17 01:11:44 +0900

    페투 클래스 생성 AIController 생성

---
+++ Source/UniversityProject/AI/UPPettuAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "UPPettuAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSITYPROJECT_API AUPPettuAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UniversityProject/Character/UPPettuCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/UPCharacterBase.h"
+#include "UPPettuCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSITYPROJECT_API AUPPettuCharacter : public AUPCharacterBase
+{
+	GENERATED_BODY()
+	
+};
