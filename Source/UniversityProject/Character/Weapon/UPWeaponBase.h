4913eaa4cb62a550c240d6c666df01241c0baf19
Kevin04261004 <kdystudy0426@gmail.com>
2024-09-19 00:38:51 +0900

    protected

--- Source/UniversityProject/Character/Weapon/UPWeaponBase.h
+++ Source/UniversityProject/Character/Weapon/UPWeaponBase.h
@@ -19,10 +19,6 @@ public:
 
 	// delegate
 	FOnWeaponHit OnWeaponHit;
-	// 상대와 충돌함.
-	void Attack(FHitResult& result);
-	// 상속받은 무기에서 공격에 성공했을 때 무기 클래스에서 필요한 작업들. 만약 구현된 함수가 필요없으면 Super::AttackSuccess()를 제외할 것.
-	virtual void AttackSuccess(FHitResult& result, class IUPDamageableInterface* Damageable);
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
 	USkeletalMeshComponent* WeaponMesh;
@@ -36,4 +32,10 @@ protected:
 	USoundBase* BaseHitSound;
 	
 	TArray<AActor*> AttackedActors;
+
+	// 상대와 충돌함.
+	void Attack(FHitResult& result);
+
+	// 상속받은 무기에서 공격에 성공했을 때 무기 클래스에서 필요한 작업들. 만약 구현된 함수가 필요없으면 Super::AttackSuccess()를 제외할 것.
+	virtual void AttackSuccess(FHitResult& result, class IUPDamageableInterface* Damageable);
 };
