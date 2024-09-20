cdc6f6ee3d01d157204a21bc19ea36be9b04ddeb
Kevin04261004 <kdystudy0426@gmail.com>
2024-09-20 18:58:05 +0900

    오류 수정 및 기본 맵 세팅

--- Source/UniversityProject/Character/Enemy/UPPettuCharacter.cpp
+++ Source/UniversityProject/Character/Enemy/UPPettuCharacter.cpp
@@ -1,15 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/UPPettuCharacter.h"
+#include "Character/Enemy/UPPettuCharacter.h"
 #include "Components/UPCharacterStatComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
-AUPPettuCharacter::AUPPettuCharacter()
+AUPPettuCharacter::AUPPettuCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.SetDefaultSubobjectClass<UUPCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
 {
 	static ConstructorHelpers::FObjectFinder<UUPCharacterStatData> StatDataRef(TEXT("/Game/UniversityProject/GameData/DA_PettuCharacterStat.DA_PettuCharacterStat"));
-	Stat->SetBaseStat(StatDataRef.Object.Get()->Stat);
-	GetCharacterMovement()->MaxWalkSpeed = Stat->GetBaseStat().MovementSpeed;
+	StatComponent->SetBaseStat(StatDataRef.Object.Get()->Stat);
+	GetCharacterMovement()->MaxWalkSpeed = StatComponent->GetBaseStat().WalkSpeed;
 	
 	MaxComboCount = 3.0f;
 	BaseComboFrameRate = 60.0f;
@@ -17,7 +17,7 @@ AUPPettuCharacter::AUPPettuCharacter()
 	MaxPatternActivateTime = 5000.0f;
 	MaxStunStack = 100.0f;
 
-	CurrentHp = Stat->GetBaseStat().MaxHp;
+	CurrentHp = StatComponent->GetBaseStat().MaxHp;
 	CurrentPatternActivateTime = 0.0f;
 	DistanceFromPlayer = 0.0f;
 	CurrentStunStack = 0.0f;
@@ -27,20 +27,20 @@ AUPPettuCharacter::AUPPettuCharacter()
 	
 	
 	// 죽는 몽타주 재설정
-	static ConstructorHelpers::FObjectFinder<UAnimMontage> DeadMontageRef(TEXT("/Game/Assets/Ancient_Golem/Demo/ThirdPersonRun_Montage.ThirdPersonRun_Montage"));
-	check(DeadMontageRef.Object != nullptr);
-	DeadMontage = DeadMontageRef.Object;
+	// static ConstructorHelpers::FObjectFinder<UAnimMontage> DeadMontageRef(TEXT("/Game/Assets/Ancient_Golem/Demo/ThirdPersonRun_Montage.ThirdPersonRun_Montage"));
+	// check(DeadMontageRef.Object != nullptr);
+	// DeadMontage = DeadMontageRef.Object;
 }
 
 void AUPPettuCharacter::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
 
-	Stat->OnHpZero.AddUObject(this, &AUPPettuCharacter::SetDead);
+	StatComponent->OnHpZero.AddUObject(this, &AUPPettuCharacter::SetDead);
 }
 
 float AUPPettuCharacter::UPTakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
-                                      AActor* DamageCauser)
+									  AActor* DamageCauser)
 {
 	return Super::UPTakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
 }
