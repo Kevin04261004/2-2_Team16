d1a05f14e2aabf98cff68f472609ebd93008e1e8
jacob015 <jacob015@naver.com>
2024-10-16 15:09:32 +0900

    이동 기능 완성

---
+++ Source/UniversityProject/AI/UPBTDecorator_BoolCheck.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Decorators/BTDecorator_BlackboardBase.h"
+#include "UPBTDecorator_BoolCheck.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSITYPROJECT_API UUPBTDecorator_BoolCheck : public UBTDecorator_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UniversityProject/AI/UPBTService_DistanceCheck.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "UPBTService_DistanceCheck.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSITYPROJECT_API UUPBTService_DistanceCheck : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
