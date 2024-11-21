1d91f03d89d8fa97ec53ed91633f54139453948b
Kevin04261004 <kdystudy0426@gmail.com>
2024-11-21 14:21:22 +0900

    빌드 뽑아봄. 에러 나서 수정쓰

--- Source/UniversityProject/Manager/UPActorSpawner.h
+++ Source/UniversityProject/Manager/UPActorSpawner.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UPActorSpawnerSubsystem.h"
 #include "Game/Stage/UPStageManager.h"
 #include "GameData/FUPSpawnActorData.h"
 #include "Subsystems/GameInstanceSubsystem.h"
@@ -20,13 +21,13 @@ public:
 	UPROPERTY()
 	TObjectPtr<UDataTable> ActorSpawnDataTable;
 
+	UPROPERTY()
+	TObjectPtr<UUPActorSpawnerSubsystem> ActorSpawnerSubsystem;
+	
 	UUPActorSpawner();
 	
 	UFUNCTION()
-	void SpawnActorsWhenStageStart(FString SpawnActorKey);
+	void SpawnActorsWhenStageStart(FString SpawnActorKey) const;
 	
 	void InitializeSpawner(AUPStageManager* StageManager);
-	
-private:
-	void SpawnActor(const FUPSpawnActorData& SpawnData);
 };
---
+++ Source/UniversityProject/Manager/UPActorSpawnerSubsystem.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameData/FUPSpawnActorData.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "UPActorSpawnerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSITYPROJECT_API UUPActorSpawnerSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnActorsWhenStageStart(FString SpawnActorKey);
+	void SpawnActor(const FUPSpawnActorData& SpawnData);
+
+private:
+	UPROPERTY()
+	UDataTable* ActorSpawnDataTable;
+};
