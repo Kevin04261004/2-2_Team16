cb980db365acb72ad90938b67ac8f6f1fed0e8c8
jacob015 <jacob015@naver.com>
2024-11-22 15:49:53 +0900

    죽는 애니메이션 오류 고침

---
+++ Source/UniversityProject/Animation/Notify/UPMonsterDeadAnimNotify.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "UPMonsterDeadAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNIVERSITYPROJECT_API UUPMonsterDeadAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
