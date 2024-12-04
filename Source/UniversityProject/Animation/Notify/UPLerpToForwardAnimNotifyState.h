5d88450ddba55b5b53f94aec834436e53253207a
Kevin04261004 <kdystudy0426@gmail.com>
2024-12-04 19:47:09 +0900

    3타 애니메이션 무기 이상하게 돌아가는거 해결 완료

--- Source/UniversityProject/Animation/Notify/UPLerpToForwardAnimNotifyState.h
+++ Source/UniversityProject/Animation/Notify/UPLerpToForwardAnimNotifyState.h
@@ -23,6 +23,9 @@ public:
 
 	UPROPERTY(EditAnywhere, Category="Init")
 	bool bLookTarget = false;
+
+	UPROPERTY(EditAnywhere, Category="Init")
+	bool bIgnoreBoss = false;
 	
 	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float InTotalDuration) override;
 	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime) override;
